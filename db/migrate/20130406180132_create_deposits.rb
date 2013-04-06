ea7d03b6b5040e133ae47004fd1ddeaadad6bbf7
tjy86 <thomasjyang@gmail.com>
2013-04-06 14:45:01 -0400

    migrations

--- db/migrate/20130406180132_create_deposits.rb
+++ db/migrate/20130406180132_create_deposits.rb
@@ -3,8 +3,8 @@ class CreateDeposits < ActiveRecord::Migration
     create_table :deposits do |t|
       t.decimal :amount
       t.date :date
-      #t.integer :user_id
-      #t.integer :club_id
+      t.integer :user_id
+      t.integer :club_id
       t.timestamps
     end
   end
