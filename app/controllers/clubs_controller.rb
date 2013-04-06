323dedbdbdc4ea912295a314b1c2b756b3b34a12
Chris Goodmacher <cgoodmac@gmail.com>
2013-04-06 17:12:07 -0400

    whitelisting attributes for mass assignment

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,9 +1,15 @@
 class ClubsController < ApplicationController
 
   def new
-
+    @club = Club.new
   end
 
+  def create
+    @club = Club.new(name: params[:club][:name])
+    # @user = current_user
+
+    binding.pry
+  end
 
   def show
 
