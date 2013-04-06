515cf7c3b3a6e77fc35c8305809b3455b4d58945
Chris Goodmacher <cgoodmac@gmail.com>
2013-04-06 14:26:34 -0400

    blah

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.11'
 gem 'pg'
 gem 'devise'
 gem 'faraday'
+gem 'finance'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/migrate/20130406180132_create_deposits.rb
+++ db/migrate/20130406180132_create_deposits.rb
@@ -1,7 +1,9 @@
 class CreateDeposits < ActiveRecord::Migration
   def change
     create_table :deposits do |t|
-
+      t.integer :user_id
+      t.integer :club_id
+      t.float :amount
       t.timestamps
     end
   end
