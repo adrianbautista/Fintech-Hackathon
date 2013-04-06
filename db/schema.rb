9c145a33ec7e187b5bfecfa3673ffa7eacf3ad8a
Adrian <adrianbautista8@gmail.com>
2013-04-06 15:05:24 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406180148) do
+ActiveRecord::Schema.define(:version => 20130406185751) do
 
   create_table "clubs", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(:version => 20130406180148) do
     t.integer  "club_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "message"
+    t.string   "status"
   end
 
   create_table "transactions", :force => true do |t|
@@ -66,5 +68,14 @@ ActiveRecord::Schema.define(:version => 20130406180148) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "request_id"
+    t.integer  "user_id"
+    t.integer  "club_id"
+    t.boolean  "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
