1e93641e8a57afbd52cb6c3b74d5f0089ecf8baf
Chris Goodmacher <cgoodmac@gmail.com>
2013-04-06 19:39:17 -0400

    more methods

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,11 +1,12 @@
 class DepositsController < ApplicationController
   
   def new
-    
+    @user = current_user
+    @club = Club.find(params[:club_id])
   end
 
-
   def create
+    @deposit = Deposit.new
     
   end
   
