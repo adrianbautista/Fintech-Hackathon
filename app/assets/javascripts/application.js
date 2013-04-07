902c569eaf59b00128070f9fc8011dd513598e41
Chris Goodmacher <cgoodmac@gmail.com>
2013-04-06 22:29:26 -0400

    stashing changes to show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require_tree .
+//= require raphael-min
+//= require morris
+//= require application
 
 $(document).foundation();
