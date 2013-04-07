c377e348247b5f49598b290fe00d2c94f7145efe
Adrian <adrianbautista8@gmail.com>
2013-04-06 21:10:04 -0400

    generate votes on request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,6 +7,10 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.create(params[:request])
+    Club.find(@request.club_id).users.each do |user|
+      Vote.create(request_id: @request.id, user_id: user.id, club_id: @request.club_id)
+    end
+    redirect_to club_path(@request.club)
   end
 
 
